e24a36e896feb289dcfbe49d60b4c23dc26f02d9
Scott Dickerson <scott.dickerson@contrastsecurity.com>
2023-05-05 13:25:29 -0600

    feat: simple socket listener

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
+// import http from 'http';
+
+import cors from 'cors';
 import express from 'express';
+import { Server } from 'socket.io';
 
 const app = express();
+app.use(cors());
+// const server = http.createServer(app);
+const io = new Server(8081);
 const PORT = 8080 || process.env.PORT;
 
 // define a route handler for the default home page
-app.get('/', (_req, res) => {
-  res.send('Hello world!');
+// app.get('/', (_req, res) => {
+//   res.send('Hello world!');
+// });
+
+app.get('/api', (_req, res) => {
+  res.send({ data: 'My data' });
+});
+
+io.on('connection', (socket) => {
+  // send a message to the client
+  socket.emit('hello from server', 1, '2', { 3: Buffer.from([4]) });
+
+  // receive a message from the client
+  socket.on('hello from client', (args) => {
+    console.log('hello from client', args);
+  });
 });
 
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`));
