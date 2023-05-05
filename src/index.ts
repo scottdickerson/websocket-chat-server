faeddb412cce9075e8cb6ae376b03373f1fc059a
Scott Dickerson <scott.dickerson@contrastsecurity.com>
2023-05-05 13:01:20 -0600

    bugfix: update with running server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export * from './lib/async';
-export * from './lib/number';
+import express from 'express';
+
+const app = express();
+const PORT = 8080 || process.env.PORT;
+
+// define a route handler for the default home page
+app.get('/', (_req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(PORT, () => console.log(`App is live on port ${PORT}`));
