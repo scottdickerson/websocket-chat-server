9538bd0372030884da695e5b1a544eebb6fb83b5
Scott Dickerson <scott.dickerson@contrastsecurity.com>
2023-05-05 14:39:24 -0600

    feat: working delayed typing response

--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,25 @@ app.get('/api', (_req, res) => {
   res.send({ data: 'My data' });
 });
 
+const typeTheResponse = (stringToSend: string) => {
+  const intervalTimer = setInterval(() => {
+    const nextChar = stringToSend[0];
+    stringToSend = stringToSend.slice(1);
+    io.emit('hello from server', nextChar);
+    if (stringToSend.length === 0) {
+      clearInterval(intervalTimer);
+    }
+  }, 100);
+};
+
 io.on('connection', (socket) => {
   // send a message to the client
   console.log('a user connected');
-  socket.emit('hello from server', "I'm the server\n");
-
+  typeTheResponse('Hello do you have a question?\n');
   // receive a message from the client
   socket.on('hello from client', (args) => {
     console.log('hello from client', args);
-    socket.emit('hello from server', `You said: ${args}\n`);
+    typeTheResponse(`\nYou said\n${args}`);
   });
 });
 
